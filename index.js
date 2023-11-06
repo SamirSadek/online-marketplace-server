75820b5263c3474099a27d53551ff9a31de1bfc3
Samir Sadek <samirsadek2009@gmail.com>
2023-11-06 11:46:05 +0600

    create database in mongoDB and insert data from server site to mongodb

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    const jobCollection = client.db('jobDB').collection('job')
     app.post('/addJob',async(req,res)=>{
         const newJob = req.body;
         console.log(newJob)
+        const result = await jobCollection.insertOne(newJob)
+        res.send(result)
     })
 
 
