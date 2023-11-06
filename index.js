a6f38b96fd23b7d24c9ad2732413e4b164515c59
Samir Sadek <samirsadek2009@gmail.com>
2023-11-06 12:22:48 +0600

    implement read operation for newJob route

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const jobCollection = client.db('jobDB').collection('job')
+
+    app.get('/addJob', async(req, res)=>{
+        const cursor = jobCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
     app.post('/addJob',async(req,res)=>{
         const newJob = req.body;
         console.log(newJob)
