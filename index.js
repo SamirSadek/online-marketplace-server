44073edb8c1c7d849bee3437c1a8eae1898b8744
Samir Sadek <samirsadek2009@gmail.com>
2023-11-07 09:45:11 +0600

    create a new dbcollection for storing the bid job info

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmjacbf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,45 +18,74 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const jobCollection = client.db('jobDB').collection('job')
+    const jobCollection = client.db("jobDB").collection("job");
+    const bidCollection = client.db("jobDB").collection("bid");
 
-    app.get('/addJob', async(req, res)=>{
-        const cursor = jobCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/addJob", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/addJob/:id', async(req, res)=>{
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await jobCollection.findOne(query)
-        res.send(result)
-    })
+    app.get("/addJob/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/addJob',async(req,res)=>{
-        const newJob = req.body;
-        console.log(newJob)
-        const result = await jobCollection.insertOne(newJob)
-        res.send(result)
-    })
+    app.post("/addJob", async (req, res) => {
+      const newJob = req.body;
+      console.log(newJob);
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+    app.post("/bidJob", async (req, res) => {
+      const bidJob = req.body;
+      console.log(bidJob);
+      const result = await bidCollection.insertOne(bidJob);
+      res.send(result);
+    });
 
-    app.delete('/addJob/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await jobCollection.deleteOne(query)
-        res.send(result)
-    })
+    app.put("/addJob/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedJob = req.body
+      const updateJob = {
+        $set: {
+          employerEmail: updatedJob.employerEmail,
+          title: updatedJob.title,
+          deadline: updatedJob.deadline,
+          category: updatedJob.category,
+          minPrice: updatedJob.minPrice,
+          maxPrice: updatedJob.maxPrice,
+          description: updatedJob.description
+        },
+      };
+      const result = await jobCollection.updateOne(filter,updateJob,options)
+      res.send(result)
+    });
+
+    app.delete("/addJob/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -67,12 +93,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Job Connect Server is Running");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send('Job Connect Server is Running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Job Connect server is running on port : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Job Connect server is running on port : ${port}`);
+});
