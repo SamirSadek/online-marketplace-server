d8f46c491b0a74951de948ccca4ef2696ed976ce
Samir Sadek <samirsadek2009@gmail.com>
2023-11-08 16:29:18 +0600

    create jwt  token for login user and delete token when user logging out

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -7,7 +8,12 @@ require("dotenv").config();
 
 // middleware
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmjacbf.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,6 +33,31 @@ async function run() {
     await client.connect();
     const jobCollection = client.db("jobDB").collection("job");
     const bidCollection = client.db("jobDB").collection("bid");
+
+
+    app.post('/jwt', async(req, res)=>{
+        const user = req.body
+        console.log('user for token', user)
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1h'})
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({success: true})
+    })
+
+
+  app.post('/logout', async(req, res)=>{
+    const user = req.body
+    console.log('logging out', user)
+    res.clearCookie('token', {maxAge: 0}).send({success: true})
+  })
+
+
+
+
+
 
     app.get("/addJob", async (req, res) => {
       const cursor = jobCollection.find();
